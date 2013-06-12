a0e74c855b29e6efb9e0fdde434cb96c3d0a6c84
VIBHOO MISHRA <vibhoo@ymail.com>
2013-06-11 23:16:17 -0500

    Final

--- vibhoo/js/hello-world.js
+++ vibhoo/js/hello-world.js
@@ -1,64 +1,62 @@
-// JavaScript Document
-// Wait for PhoneGap to load
-document.addEventListener("deviceready", onDeviceReady, false);
+$(document).delegate('a', 'click', function () {
+    $('.content_div').hide();
+    $('#' + $(this).attr('data-href')).show();
+});
 
-// PhoneGap is ready
-function onDeviceReady() {
-    getLocation();
-    navigator.splashscreen.hide();
-}
+// Checking of inputting only number
+$(document).ready(function () {
+ $("#m_number").keypress(function(e) {
+   if(e.which != 8 && e.which !=0 &&(e.which <48 || e.which >57)) {
+      $("#errmsg").html("Digital Only").show().fadeOut("slow");
+       return false;
+       }
+   }); 
+}); 
 
-function getLocation() {
-    myNewFunction();
-}
-  
-function myNewFunction(){
-    navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError);
-}
-  
-//=======================Say Hello (Page 1) Operations=======================//
-function sayHello() {
-    var sayHelloInputElem = document.getElementById('helloWorldInput');
-    var sayHelloTextElem = document.getElementById('helloWorldText');
-    var inputText = document.getElementById('txtName');
-    
-    sayHelloTextElem.innerHTML = 'Hello, ' + inputText.value + '!';
-    sayHelloTextElem.style.display = 'block';
-    sayHelloInputElem.style.display = 'none';
-}
+$(document).delegate('a', 'click',function() {
+    $('#dc,#cc').hide();
+    $('#'+$(this).attr('data-href')).show();
+});
 
-function sayHelloReset() {
-    var sayHelloInputElem = document.getElementById('helloWorldInput');
-    var sayHelloTextElem = document.getElementById('helloWorldText');
-    var inputText = document.getElementById('txtName');
-    
-    inputText.value = '';
-    sayHelloTextElem.style.display = 'none';
-    sayHelloInputElem.style.display = 'block';
-}
+$(document).delegate('a', 'click',function() {
+    $('#dc,#nb').hide();
+    $('#'+$(this).attr('data-href')).show();
+}); 
 
-//=======================Geolocation Operations=======================//
-// onGeolocationSuccess Geolocation
-function onGeolocationSuccess(position) {
-    // Use Google API to get the location data for the current coordinates
-    var geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    geocoder.geocode({ "latLng": latlng }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            if ((results.length > 1) && results[1]) {
-                $("#myLocation").html(results[1].formatted_address);
-            }
-        }
-    });
-    
-    // Use Google API to get a map of the current location
-    // http://maps.googleapis.com/maps/api/staticmap?size=280x300&maptype=hybrid&zoom=16&markers=size:mid%7Ccolor:red%7C42.375022,-71.273729&sensor=true
-    var googleApis_map_Url = 'http://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=hybrid&zoom=16&sensor=true&markers=size:mid%7Ccolor:red%7C' + latlng;
-    var mapImg = '<img src="' + googleApis_map_Url + '" />';
-    $("#map_canvas").html(mapImg);
-}
+$(document).delegate('a', 'click',function() {
+    $('#nb,#cc').hide();
+    $('#'+$(this).attr('data-href')).show();
+});
 
-// onGeolocationError Callback receives a PositionError object
-function onGeolocationError(error) {
-    $("#myLocation").html("<span class='err'>" + error.message + "</span>");
-}
+$(document).delegate('a', 'click',function() {
+    $('#success,#fail').hide();
+    $('#'+$(this).attr('data-href')).show();
+});
+
+$(document).delegate('a', 'click',function() {
+    $('#all,#fail').hide();
+    $('#'+$(this).attr('data-href')).show();
+});
+
+
+$(document).delegate('a', 'click',function() {
+    $('#all,#success').hide();
+    $('#'+$(this).attr('data-href')).show();
+});
+
+
+
+$(document).delegate('a', 'click',function() {
+    $('#faq,#t_c').hide();
+    $('#'+$(this).attr('data-href')).show();
+});
+
+$(document).delegate('a', 'click',function() {
+    $('#t_c,#he').hide();
+    $('#'+$(this).attr('data-href')).show();
+});
+
+$(document).delegate('a', 'click',function() {
+    $('#faq,#he').hide();
+    $('#'+$(this).attr('data-href')).show();
+});
